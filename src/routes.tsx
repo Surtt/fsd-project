acb9897af73cb8e27e6f6fd0660f784c0bb820ce
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2025-06-11 23:20:21 +0300

    fix: remove index.css

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,17 +5,17 @@ export const router = createBrowserRouter([
     path: "/",
     /* Component: Layout, */
     children: [
-      { index: true, element: <a>Main</a> },
-      { path: "community", element: <a>Community</a> },
+      { index: true, element: <div>Main</div> },
+      { path: "community", element: <div>Community</div> },
       {
         path: "article",
-        children: [{ path: ":alias", element: <a>Article</a> }],
+        children: [{ path: ":alias", element: <div>Article</div> }],
       },
       {
         path: "profile",
         children: [
-          { index: true, element: <a>Profile</a> },
-          { path: "edit", element: <a>EditProfile</a> },
+          { index: true, element: <div>Profile</div> },
+          { path: "edit", element: <div>EditProfile</div> },
         ],
       },
     ],
